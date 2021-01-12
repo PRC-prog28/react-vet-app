8c5039c3b49e260569d026c98b5a3d970f389dee
PRC-prog28 <rcpallavi2019@gmail.com>
2021-01-12 23:44:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-vet-app-stw3sn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vet-app-stw3sn)
---
+++ src/components/ListAppointments.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { FaTimes } from "react-icons/fa";
+import Moment from "react-moment";
+
+class ListAppointments extends Component {
+  render() {
+    return (
+      <div className="appointment-list item-list mb-3">
+        {this.props.appointments.map(apt => (
+          <div className="pet-item col media py-3" key={apt.aptId}>
+            <div className="mr-3">
+              <button
+                className="pet-delete btn btn-sm btn-danger"
+                onClick={() => this.props.deleteAppointment(apt)}
+              >
+                <FaTimes />
+              </button>
+            </div>
+            <div className="pet-info media-body">
+              <div className="pet-head d-flex">
+                <span
+                  className="pet-name"
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={e =>
+                    this.props.updateInfo(
+                      "petName",
+                      e.target.innerText,
+                      apt.aptId
+                    )
+                  }
+                >
+                  {apt.petName}
+                </span>
+                <span className="apt-date ml-auto">
+                  <Moment
+                    date={apt.aptDate}
+                    parse="YYYY-MM-dd hh:mm"
+                    format="MMM-d h:mma"
+                  />
+                </span>
+              </div>
+              <div className="owner-name">
+                <span className="label-item">Owner: </span>
+                <span
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={e =>
+                    this.props.updateInfo(
+                      "ownerName",
+                      e.target.innerText,
+                      apt.aptId
+                    )
+                  }
+                >
+                  {apt.ownerName}
+                </span>
+              </div>
+              <div
+                className="apt-notes"
+                contentEditable
+                suppressContentEditableWarning
+                onBlur={e =>
+                  this.props.updateInfo(
+                    "aptNotes",
+                    e.target.innerText,
+                    apt.aptId
+                  )
+                }
+              >
+                {apt.aptNotes}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ListAppointments;
